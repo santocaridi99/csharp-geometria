fe88fc9d20b5c6cac6bd59606db52072969126e0
santocaridi99 <santocaridi99@gmail.com>
2022-05-12 18:12:49 +0200

    Aggiungere i file di progetto.

---
+++ csharp-geometria/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace csharp_geometria{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valore1,valore2;
+            Console.WriteLine("inserire valore base  rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out valore1))
+            {
+                Console.WriteLine("Non hai inserito un numero");
+                Environment.Exit(1);
+
+            }
+            Console.WriteLine("inserire valore altezza rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out valore2))
+            {
+                Console.WriteLine("Non hai inserito un numero");
+                Environment.Exit(1);
+
+            }
+
+            Console.WriteLine("inserire nome Rettangolo");
+            string nome1 = Console.ReadLine();
+
+
+            Rettangolo Rettangolo1 = new Rettangolo(valore1, valore2,nome1);
+            Rettangolo1.StampaRettangolo();
+            Rettangolo1.disegna();
+
+
+            //Rettangolo Rettangolo2 = new Rettangolo(30, 40);
+            //Rettangolo2.calcolaPerimetro();
+            //Rettangolo2.calcolaArea();
+            int valore3, valore4;
+            Console.WriteLine("inserire valore base  rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out valore3))
+            {
+                Console.WriteLine("Non hai inserito un numero");
+                Environment.Exit(1);
+
+            }
+            Console.WriteLine("inserire valore altezza rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out valore4))
+            {
+                Console.WriteLine("Non hai inserito un numero");
+                Environment.Exit(1);
+
+            }
+            Console.WriteLine("inserire nome Rettangolo");
+            string nome2 = Console.ReadLine();
+            Rettangolo Rettangolo2 = new Rettangolo(valore3, valore4,nome2);
+            Rettangolo2.StampaRettangolo();
+            Rettangolo2.disegna();
+
+        }
+    }
+
+    }
---
+++ csharp-geometria/Rettangolo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+        private string nomeRettangolo;
+
+        //costruttore
+        public Rettangolo(int @base , int altezza , string name)
+        {
+            this.baseRettangolo = @base;
+            this.altezzaRettangolo = altezza;
+            this.nomeRettangolo = name;
+        }
+
+        public int calcolaArea()
+        {
+            int area = baseRettangolo * altezzaRettangolo;
+            return area;
+        }
+
+        public double calcolaPerimetro()
+        {
+            int perimetro = baseRettangolo * 2 + altezzaRettangolo * 2;
+            return perimetro;
+        }
+
+
+        public void StampaRettangolo()
+        {
+            Console.WriteLine(" rettangolo: {0}", nomeRettangolo);
+            Console.WriteLine("base: {0} cm", baseRettangolo);
+            Console.WriteLine("altezza: {0} cm", altezzaRettangolo);
+            Console.WriteLine("Perimetro: {0} cm", calcolaPerimetro());
+            Console.WriteLine("Area: {0} cm", calcolaArea());
+            Console.WriteLine();
+        }
+
+        //bonus
+        public void disegna()
+        {
+            Console.Write("+");
+            for (int i = 1; i < (baseRettangolo - 1); ++i)
+                Console.Write("-");
+            Console.WriteLine("+");
+
+            for (int i = 1; i < (altezzaRettangolo - 1); ++i)
+                Console.WriteLine("|{0}|", new String(' ', (int)(baseRettangolo - 2)));
+
+            Console.Write("+");
+            for (int i = 1; i < (baseRettangolo - 1); ++i)
+                Console.Write("-");
+            Console.WriteLine("+");
+        }
+    }
+}
